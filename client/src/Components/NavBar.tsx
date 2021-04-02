22328be5de3edb2c790b2cb8211bf901764ebcd8
Barbara Diaz <barbarad91@gmail.com>
2021-04-02 20:48:54 +0200

    Modify Navbar

--- client/src/Components/NavBar.tsx
+++ client/src/Components/NavBar.tsx
@@ -2,129 +2,69 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import IconButton from '@material-ui/core/IconButton'
+// import IconButton from '@material-ui/core/IconButton'
 import Icon from '@mdi/react'
-import { mdiMenu, mdiChevronLeft } from '@mdi/js'
+import { mdiAccount, mdiLogoutVariant } from '@mdi/js'
 import { useState } from 'react'
-import { Drawer } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 import { useLoggedUserContext } from 'src/Pages/LoggedUserContext'
 import clsx from 'clsx'
-import BarMenu from './BarMenu'
+import AuthService from 'src/Services/auth.service'
+import { Button, IconButton } from '@material-ui/core'
 
 const NavBar = () => {
   const classes = useStyles()
 
-  const { user } = useLoggedUserContext()
+  const { user, setUser } = useLoggedUserContext()
 
-  const [open, setOpen] = useState(false)
+  const authService = new AuthService()
 
-  const handleClickMenu = () => {
-    setOpen(!open)
+  const signOut = async () => {
+    await authService.signOut()
+    setUser(undefined)
   }
 
   return (
     <div className={classes.root}>
-      <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+      <AppBar position="absolute" className={classes.appBar}>
         <Toolbar className={classes.toolbar}>
-          {user && (
-            <>
-              <IconButton
-                edge="start"
-                className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-                color="inherit"
-                aria-label="menu"
-                onClick={handleClickMenu}
-              >
-                <Icon path={mdiMenu} size={1} />
-              </IconButton>
-            </>
-          )}
           <Typography variant="h6" className={classes.title} component={Link} to="/">
             Goxo
           </Typography>
+          {user && (
+            <>
+              <Link to="/profile" className={classes.linkIcon}>
+                <Icon path={mdiAccount} size={1}></Icon>
+              </Link>
+              <IconButton onClick={signOut}>
+                <Icon path={mdiLogoutVariant} size={1} />
+              </IconButton>
+            </>
+          )}
         </Toolbar>
       </AppBar>
-
-      {user && (
-        <Drawer
-          variant="permanent"
-          classes={{ paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose) }}
-          open={open}
-        >
-          <div className={classes.toolbarIcon}>
-            <IconButton onClick={handleClickMenu}>
-              <Icon path={mdiChevronLeft} size={1} />
-            </IconButton>
-          </div>
-          <BarMenu />
-        </Drawer>
-      )}
     </div>
   )
 }
 
-const useStyles = makeStyles(({ breakpoints, mixins, palette, spacing, transitions, zIndex }: Theme) =>
+const useStyles = makeStyles(({ palette, spacing }: Theme) =>
   createStyles({
     root: {
       display: 'flex',
     },
     toolbar: {
-      paddingRight: spacing(3), // keep right padding when drawer closed
-    },
-    toolbarIcon: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'flex-end',
-      padding: '0 8px',
-      ...mixins.toolbar,
+      paddingRight: spacing(3),
     },
     appBar: {
-      backgroundColor: palette.secondary.main,
-      zIndex: zIndex.drawer + 1,
-      transition: transitions.create(['width', 'margin'], {
-        easing: transitions.easing.sharp,
-        duration: transitions.duration.leavingScreen,
-      }),
-    },
-    appBarShift: {
-      marginLeft: spacing(30),
-      width: `calc(100% - ${spacing(30)}px)`,
-      transition: transitions.create(['width', 'margin'], {
-        easing: transitions.easing.sharp,
-        duration: transitions.duration.enteringScreen,
-      }),
-    },
-    menuButton: {
-      marginRight: spacing(4),
-    },
-    menuButtonHidden: {
-      display: 'none',
+      // backgroundColor: palette.secondary.main,
     },
     title: {
       flexGrow: 1,
       textDecoration: 'none',
       color: 'inherit',
     },
-    drawerPaper: {
-      position: 'relative',
-      whiteSpace: 'nowrap',
-      width: spacing(30),
-      transition: transitions.create('width', {
-        easing: transitions.easing.sharp,
-        duration: transitions.duration.enteringScreen,
-      }),
-    },
-    drawerPaperClose: {
-      overflowX: 'hidden',
-      transition: transitions.create('width', {
-        easing: transitions.easing.sharp,
-        duration: transitions.duration.leavingScreen,
-      }),
-      width: spacing(7),
-      [breakpoints.up('sm')]: {
-        width: spacing(9),
-      },
+    linkIcon: {
+      color: 'inherit',
     },
   })
 )
