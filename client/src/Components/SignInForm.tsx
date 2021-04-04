3f7b5865bd65950e4ffc0c65f3c647f6bcb0145c
Barbara Diaz <barbarad91@gmail.com>
2021-04-04 20:24:29 +0200

    Remove unnecessary imports

--- client/src/Components/SignInForm.tsx
+++ client/src/Components/SignInForm.tsx
@@ -1,7 +1,5 @@
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import Checkbox from '@material-ui/core/Checkbox'
 import React, { useState } from 'react'
 import AuthService from 'src/services/auth.service'
 import { useLoggedUserContext } from 'src/pages/LoggedUserContext'
