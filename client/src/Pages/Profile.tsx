90ac248faec7d3dc50ec5e25a187643aa20d1d6e
Barbara Diaz <barbarad91@gmail.com>
2021-04-05 11:20:48 +0200

    Add style to Profile Page

--- client/src/Pages/Profile.tsx
+++ client/src/Pages/Profile.tsx
@@ -1,4 +1,4 @@
-import { Container, makeStyles, Theme } from '@material-ui/core'
+import { Grid, makeStyles, Paper, Theme } from '@material-ui/core'
 import LoadingScreen from 'src/shared/LoadingScreen'
 import { useLoggedUserContext } from './LoggedUserContext'
 import Typography from '@material-ui/core/Typography'
@@ -13,11 +13,19 @@ const Profile = () => {
       {loading ? (
         <LoadingScreen />
       ) : (
-        <Container className={classes.root}>
-          <Typography>Email: {user?.username}</Typography>
-          <Typography>Name: {user?.name}</Typography>
-          <img src={user?.imageUrl} alt={user?.name} className={classes.image}></img>
-        </Container>
+        <Grid container className={classes.root}>
+          <Grid item xs={12} sm={6} lg={3} className={classes.gridItem}>
+            <Paper className={classes.paperCard}>
+              <Typography variant="h5" component="h2" gutterBottom>
+                Email: {user?.username}
+              </Typography>
+              <Typography variant="h5" component="h2" gutterBottom>
+                Name: {user?.name}
+              </Typography>
+              <img src={user?.imageUrl} alt={user?.name} className={classes.image}></img>
+            </Paper>
+          </Grid>
+        </Grid>
       )}
     </>
   )
@@ -27,14 +35,19 @@ const useStyles = makeStyles(({ spacing }: Theme) => ({
   root: {
     marginTop: spacing(20),
     display: 'flex',
-    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  gridItem: {
+    display: 'flex',
     alignItems: 'center',
   },
-  imageContainer: {
-    width: spacing(30),
-    height: spacing(30),
-    backgroundColor: 'black',
-    borderRadius: '50%',
+  paperCard: {
+    height: spacing(50),
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   image: {
     width: spacing(30),
